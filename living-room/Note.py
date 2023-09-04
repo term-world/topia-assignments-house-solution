6a9cc39589d7baa0e6d5760ea41c176126893140
Douglas Luman <dluman@allegheny.edu>
2023-09-03 21:53:52 -0400

    Update Note.py

--- living-room/Note.py
+++ living-room/Note.py
@@ -5,16 +5,15 @@ import os
 n = narrator.Narrator()
 
 def main():
-
     unpacked = Checkpoint.check_flag("boxes_unpacked")
     lease_printed = bool(Checkpoint.check_flag("lease_printed"))
     
     if unpacked == 5:
-        n.path.change(0.6)
+        n.path.change({"act": 0, "scene": 6})
         n.narrate()
         q = narrator.YesNoQuestion({
             "prompt":"Throw away the note", 
-            "outcomes":[0.7, 0.8]
+            "outcomes":[{"act": 0, "scene": 7}, {"act": 0, "scene": 8}]
         })
         n.path.change(q.ask())
         if n.path.scene == 0.7:
@@ -23,15 +22,15 @@ def main():
         return
 
     if bool(Checkpoint.check_flag("note_read")):
-        n.path.change(0.8)
+        n.path.change({"act": 0, "scene": 8})
         n.narrate()
         q = narrator.Question({
             "question": "Is there a certain section you want to read",
             "responses": [
-                {"choice": "unpacking", "outcome": 0.1},
-                {"choice": "lease", "outcome": 0.2},
-                {"choice": "conclusion", "outcome": 0.3},
-                {"choice": "don't read", "outcome": 0.4}
+                {"choice": "unpacking", "outcome": {"act": 0, "scene": 1}},
+                {"choice": "lease", "outcome": {"act": 0, "scene": 2}},
+                {"choice": "conclusion", "outcome": {"act": 0, "scene": 3}},
+                {"choice": "don't read", "outcome": {"act": 0, "scene": 4}}
             ]
         })
         n.path.change(q.ask())
@@ -41,18 +40,26 @@ def main():
     n.narrate()
     q = narrator.YesNoQuestion({
         "question": "Read the note",
-        "outcomes": [0.1, 0.4]
+        "outcomes": [{"act": 0, "scene": 1}, {"act": 0, "scene": 4}]
     })
     n.path.change(q.ask())
     n.narrate()
+    if n.path.scene == 5:
+        return
     while n.path.scene < 6:
         q = narrator.YesNoQuestion({
             "question": "Continue reading",
-            "outcomes": [f"0.{n.path.scene}", 0.4]
+            "outcomes": [{"act": 0, "scene": n.path.scene}, {"act": 0, "scene": 4}]
         })
         n.path.change(q.ask())
         n.narrate()
-    Checkpoint.set_flag("note_read", True)
+        if n.path.scene == 3:
+            n.narrate()
+            Checkpoint.set_flag("note_read", True)
+            n.narrate()
+            return
+        if n.path.scene == 5:
+            return
 
 if __name__ == "__main__":
     main()
